29b9b037fae3ae405a9f59279f19157f072e5dee
Tatsuya Ishikawa <tatsuyai713@gmail.com>
2024-09-23 01:31:33 +0900

    [Fix] fix script

--- install_xfce.sh
+++ install_xfce.sh
@@ -61,7 +61,6 @@ fi
 xfconf-query --channel thunar --property /misc-exec-shell-scripts-by-default --create --type bool --set true
 
 mkdir -p /home/root 
-cp -R /root/* /home/root/ 
 chown -R root:root /home/root
 sed -i 's|root:x:0:0:root:/root:|root:x:0:0:root:/home/root:|' /etc/passwd
 
